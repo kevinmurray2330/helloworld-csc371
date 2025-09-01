b9f752536cf79b09423de76f8ace4512f0b9b5c4
Kevin Murray <kevinmurray2330@gmail.com>
2025-09-01 13:38:59 -0400

    Changed text to Hell world

--- app/src/main/java/com/example/helloworld_csc371/MainActivity.kt
+++ app/src/main/java/com/example/helloworld_csc371/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Helloworldcsc371Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "World",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
